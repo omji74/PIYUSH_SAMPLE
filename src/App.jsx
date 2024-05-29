f8cc6fc6d7a0afd7cd5cbad8c1e39a197725061b
omdwd74 <omjidwivedi123@gmail.com>
2024-05-29 23:44:13 +0530

    piyush

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Piyush from './components/Piyush'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+      <Piyush/>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Piyush.jsx
@@ -0,0 +1,10 @@
+import './Piyush.css'
+const Piyush = () => {
+    return (
+        <>
+        <h1> Hii Piyush</h1>
+        <button>Submit</button>
+        </>
+    )
+}
+export default Piyush;
